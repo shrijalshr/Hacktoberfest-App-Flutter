53793e881d55d04641ec7e71109f6d27edc9e7de
Addepalli N M PavanKalyan <57014239+pavankalyan0424@users.noreply.github.com>
2021-10-09 09:24:09 +0530

    added my name

---
+++ Contributors/pavankalyan0424.md
@@ -0,0 +1,3 @@
+Name: Addepalli N M Pavan Kalyan
+GitHub Username: pavankalyan0424
+Quote: Don't Stop Until You're Proud
--- lib/contributors.dart
+++ lib/contributors.dart
@@ -28,6 +28,7 @@ class Contributers {
     'Aman-Ladla',
     'Palak-Yadav',
     'PRAJJWALmehta',
+    'pavankalyan0424'
     // Add your GitHub Username here (please add "," also)
   ];
 
