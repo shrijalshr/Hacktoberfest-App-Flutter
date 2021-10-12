56ba2a2eed825fc233607da69f31a9ca446451c1
Harmanjit Singh <harmanjit140500@gmail.com>
2021-10-13 05:22:49 +0530

    url_launcher added

--- lib/Screens/tile.dart
+++ lib/Screens/tile.dart
@@ -1,41 +1,51 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Tile extends StatelessWidget {
-  String name;
-  Tile({required this.name});
+  final String name;
+  const Tile({required this.name});
+
+  void launcher() async {
+    final String _url = "https://github.com/" + name;
+    await canLaunch(_url) ? await launch(_url) : throw 'Could not launch $_url';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 10.0),
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(
-            20,
+      padding: const EdgeInsets.only(top: 10.0, left: 5, right: 5),
+      child: InkWell(
+        onTap: launcher,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              20,
+            ),
           ),
-        ),
-        color: Color(0xFFF2E88B),
-        elevation: 5,
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Row(
-            children: [
-              Image.asset(
-                'images/githubnrb.png',
-                scale: 5,
-              ),
-              SizedBox(
-                width: 15,
-              ),
-              Text(
-                name,
-                style: TextStyle(
-                  fontSize: 30,
-                  fontFamily: 'Satisfy',
+          color: Color(0xFFF2E88B),
+          elevation: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                Image.asset(
+                  'images/githubnrb.png',
+                  scale: 5,
                 ),
-              ),
-            ],
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  name,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontFamily: 'Satisfy',
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
